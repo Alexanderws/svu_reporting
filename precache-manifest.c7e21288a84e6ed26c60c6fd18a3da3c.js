0aa8ffb1ed2240f40738417eee977bdaeeaea1fb
alexanderws <Alex.ws@icloud.com>
2020-04-01 17:43:51 +0200

    Updates

---
+++ precache-manifest.c7e21288a84e6ed26c60c6fd18a3da3c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff830d4fa6c9b8d9b9b069c557bac2e4",
+    "url": "/svu_reporting/index.html"
+  },
+  {
+    "revision": "2ae1e4c054551743d258",
+    "url": "/svu_reporting/static/css/main.974058b2.chunk.css"
+  },
+  {
+    "revision": "1c458d538c3e63b67fbb",
+    "url": "/svu_reporting/static/js/2.11ed3a81.chunk.js"
+  },
+  {
+    "revision": "bf8f7d802c27a73a544775ad82161761",
+    "url": "/svu_reporting/static/js/2.11ed3a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae1e4c054551743d258",
+    "url": "/svu_reporting/static/js/main.58c6d788.chunk.js"
+  },
+  {
+    "revision": "b3113cad611f72eff0e6",
+    "url": "/svu_reporting/static/js/runtime-main.98488e4d.js"
+  },
+  {
+    "revision": "183cd7db06b21f0bc404c7d39cfdaee0",
+    "url": "/svu_reporting/static/media/OsloSans-Bold.183cd7db.woff"
+  },
+  {
+    "revision": "e6710c598a1f6dfdf6f0bb3e8b6cb6bf",
+    "url": "/svu_reporting/static/media/OsloSans-BoldItalic.e6710c59.woff"
+  },
+  {
+    "revision": "a9551ce01fc7f9f8c24adf23e9eebfe4",
+    "url": "/svu_reporting/static/media/OsloSans-Light.a9551ce0.woff"
+  },
+  {
+    "revision": "d644b088cefaef70768514309328d5b9",
+    "url": "/svu_reporting/static/media/OsloSans-LightItalic.d644b088.woff"
+  },
+  {
+    "revision": "5d3dc5b36a0e033375626e4f5c0d0967",
+    "url": "/svu_reporting/static/media/OsloSans-Medium.5d3dc5b3.woff"
+  },
+  {
+    "revision": "3bf5f0817312ccc1e66422e24375953e",
+    "url": "/svu_reporting/static/media/OsloSans-MediumItalic.3bf5f081.woff"
+  },
+  {
+    "revision": "4ee6a819b3279d0525e2884d013e85d9",
+    "url": "/svu_reporting/static/media/OsloSans-Regular.4ee6a819.woff"
+  },
+  {
+    "revision": "1b164fe01629d9d67f01440ae56f0d00",
+    "url": "/svu_reporting/static/media/OsloSans-RegularItalic.1b164fe0.woff"
+  }
+]);
